f8525bf1b56431b050bbf1dbf0f104545dddbd31
polskibobea <lubiebobea@gmail.com>
2026-09-15 19:02:37 +0200

    safda

--- hosts/minimal/configuration.nix
+++ hosts/minimal/configuration.nix
@@ -4,6 +4,9 @@
   boot.zfs.forceImportRoot = false;
   nix.settings.experimental-features = ["flakes" "nix-command"];
   i18n.defaultLocale = "pl_PL.UTF-8";
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+  system.stateVersion = "26.05";
 }
