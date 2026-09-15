4e1fd99aefda3d5c45077921e037a97fcc3dc39a
polskibobea <lubiebobea@gmail.com>
2026-09-15 18:22:06 +0200

    zzzz

--- hosts/minimal/configuration.nix
+++ hosts/minimal/configuration.nix
@@ -4,4 +4,6 @@
   boot.zfs.forceImportRoot = false;
   nix.settings.experimental-features = ["flakes" "nix-command"];
   i18n.defaultLocale = "pl_PL.UTF-8";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
 }
